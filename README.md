d86ee3e428f12d17910e6a697cb7f9b447c1600d
Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>
2020-08-13 19:17:07 +0530

    Add README
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipulgupta2048@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# File Downloads Demo with WebdriverIO
+
+Since, there is no one way to make file downloads work in WebdriverIO with headless chromium using the Devtools protocol, I wrote about a blog post detailing what worked for me along with the sample code hosted on this repository. 
+
+![](a.png)
+
+For any questions, you can reach out to me on [Twitter](https://twitter.com/vipulgupta2048) or [comment down on the post](https://mixstersite.wordpress.com/2020/08/13/file-download-headless-chrome-chromium-webdriverio-devtools-chromedriver/) and I would reply under a day or so. Toodles and hope this solves your issue.  
--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,13 +1,17 @@
-describe('webdriver.io page', () => {
+describe('Testing file downloads in headless browser', () => {
     it('should have the right title', () => {
         // Open the URL
         browser.url('http://www.fileformatcommons.com/txt-file-format/')
+        
         // Set the download behavior of the page right before the download
         browser.cdp('Page', 'setDownloadBehavior', {
             behavior: 'allow',
             downloadPath: '/home/vipulgupta2048/webd',
         });
+        
+        // Clicking the download button
         $('.wp-image-36').click()
+        
         // Give enough time to let the file download
         browser.pause(5000)
     })
